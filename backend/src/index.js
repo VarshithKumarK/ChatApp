7bf5df350f52cc80f056a46adfa2120b5547f067
Varshith Kumar K <varshithkumark983@gmail.com>
2025-03-27 15:52:09 +0530

    Commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,8 @@ import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
-
+import path from "path";
+const __dirname = path.resolve();
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -23,7 +24,15 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(PORT, () => {
   console.log("Server is Running on PORT :", PORT);
   connectDB();
 });
+               
